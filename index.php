b695cf0f2676f97a66e6ede2ea06571a9685f576
Mason Toy <mason.toy@gmail.com>
2017-03-27 10:30:42 -0400

    Add to GitHub
    Had this site for a while, figured I should probably push to github

---
+++ index.php
@@ -0,0 +1,252 @@
+<!DOCTYPE HTML>
+<!--
+	Hyperspace by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<title>Mason Toy</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<link rel="shortcut icon" type="image/x-icon" href="favicon.png" />
+		<!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
+		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+	</head>
+	<body>
+
+		<!-- Scripts -->
+			<script>
+			  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+			  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+			  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+			  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+			  ga('create', 'UA-86073703-1', 'auto');
+			  ga('send', 'pageview');
+
+			</script>
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.scrollex.min.js"></script>
+			<script src="assets/js/jquery.scrolly.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+			<script src="assets/js/main.js"></script>
+
+
+		<!-- Sidebar -->
+			<section id="sidebar">
+				<div class="inner">
+					<nav>
+						<ul>
+							<li><a href="#intro">Welcome</a></li>
+							<li><a href="#one">Who am I?</a></li>
+							<li><a href="#two">What do I do?</a></li>
+							<li><a href="#three">Projects</a></li>
+							<li><a href="#four">Contact me!</a></li>
+						</ul>
+					</nav>
+				</div>
+			</section>
+
+		<!-- Wrapper -->
+			<div id="wrapper">
+
+				<!-- Intro -->
+					<section id="intro" class="wrapper style1 fullscreen fade-up">
+						<div class="inner">
+							<h1>Mason Toy</h1>
+							<p>Hey, Welcome to my website! This site is a way for you to get to know me and what I am all about. My resume is available in the <a href="#four" class="scrolly">'Contact me!'</a> section. Feel free to contact me about anything you are curious about!</p>
+							<ul class="actions">
+								<li><a href="#one" class="button scrolly">Who Am I?</a></li>
+							</ul>
+						</div>
+					</section>
+
+				<!-- One -->
+					<section id="one" class="wrapper style2 spotlights">
+						<section>
+							<div class="content">
+								<div class="inner">
+									<h2>Who is Mason Toy?</h2>
+									<p>I am a senior in Software Engineering at Penn State Erie, The Behrend College. When I am at school I am the current president of the ACM club as well as a member in the Game Developers club. I truly enjoy being able to make helpful applications come to life. I have attended many hackathons around the area such as, Hack PSU, Brickhack, and MLH's Local Hack Day which was sponsored our ACM club here at PSB. I have a wide range of experience ranging from QA at Club Prophet Systems, to working at an advertising agency called Gatesman+Dave building Websites and Applications for clients.</p>
+								</div>
+							</div>
+							<a class="image"><img src="images/my_headshot.jpg" alt="" data-position="25% 25%" /></a>
+						</section>
+					</section>
+
+				<!-- Two -->
+					<section id="two" class="wrapper style3 fade-up">
+						<div class="inner">
+							<h2>What do I do?</h2>
+							<p>Here are some concepts and skills that I excel at.</p>
+							<div class="features">
+								<section>
+									<span class="icon major fa-code"></span>
+									<h3>Programing Languages</h3>
+									<div style="width: 100%; display: inline-block;">
+										<div class="list-split-left" style="height: auto; width: 50%; float: left;">
+											<ul>
+												<li>C</li>
+												<li>C#</li>
+												<li>C++</li>
+												<li>Java</li>
+												<li>MIPS Assembly</li>
+											</ul>
+										</div>
+										<div class="list-split-right" style="height: auto; width: 50%; float: right;">
+											<ul>
+												<li>JavaScript</li>
+												<li>JQuery</li>
+												<li>CSS</li>
+												<li>HTML</li>
+												<li>SCSS</li>
+											</ul>
+										</div>
+									</div>
+								</section>
+								<section>
+									<span class="icon major fa-thumbs-o-up"></span>
+									<h3>Programing Concepts & skills</h3>
+									<div style="width: 100%; display: inline-block;">
+										<div class="list-split-left" style="width: 50%; float: left;">
+											<ul>
+												<li>Algorithm Analysis</li>
+												<li>Project Design (UML)</li>
+												<li>Object Oriented Design</li>
+												<li>Software Architecture and Design patterns</li>
+												<li>Software Documentation Process</li>
+											</ul>
+										</div>
+										<div class="list-split-right" style="width: 50%; float: right;">
+											<ul>
+												<li>Relational Databases</li>
+												<li>Unit Testing</li>
+												<li>Regression Testing</li>
+												<li>Computer Networking</li>
+												<li>Agile Development</li>
+											</ul>
+										</div>
+									</div>
+								</section>
+							</div>
+						</div>
+					</section>
+
+					<section id="three" class="wrapper style2-alt spotlights">
+						<section>
+							<div class="content">
+								<div class="inner">
+									<h1>Projects</h1>
+								</div>
+							</div>
+						</section>
+					</section>
+					<section class="wrapper style2 spotlights">
+						<section>
+						<a class="image"><img src="images/MuZe.png" alt="" /></a>
+							<div class="content">
+								<div class="inner">
+									<h2>MuZe Streaming</h2>
+									<p>For this project my team used Java to implement a streaming server and client. This program uses RTSP, RTP and FTP to allow the users to upload, download, listen to the music at the same time and enables the users to chat to each other. Another challenge our team had with this project was supporting MP3s as Java does not natively support streaming MP3 format. Overall, this Project was a ton of fun to work on and a great challenge to learn all the different internet protocols.</p>
+								</div>
+							</div>
+						</section>
+					</section>
+					<section class="wrapper style2-alt spotlights">
+						<section>
+							<div class="content">
+								<div class="inner">
+									<h2>Gatesman+Dave Brandtern Experience Webpage</h2>
+									<p>While interning at Gatesman+Dave the summer Brandtern Class a project to increase the awareness of the Brandtern program to increase the pool of applicants. One of the things we decided was to rework the webpage. This webpage was completed by me. I used JQuery, JavaScript, SASS, PHP and SVG image code. The website itself is using WordPress however; most of what I changed was behind the scenes with in the templates code and changing a lot of the JQuery.</p>
+									<a href="http://gatesmandave.com/about/brandtern/" target="_Blank" class="button">The Brandtern Experience</a>
+								</div>
+							</div>
+							<a class="image"><img src="images/BrandternWebpage.PNG" alt="" /></a>
+						</section>
+					</section>
+					<section class="wrapper style2 spotlights">
+						<section>
+						<a class="image"><img src="images/VisuSort_MIPS.PNG" alt=""/></a>
+							<div class="content">
+								<div class="inner">
+									<h2>VisuSort</h2>
+									<p>is a MIPS assembly, made and ran inside of MARS, program that displays different sorting algorithms. The algorithms that will be available are: Insertion, Bubble Sort, Quick Sort, Selection Sort. The user will have the ability to choose which sort they want to see visualized. The program will then move through the sorting process through each iteration. Each time that a new number moved the graph will be updated with the current values in order in the number array.</p>
+								</div>
+							</div>
+						</section>
+					</section>
+					<section class="wrapper style2-alt spotlights">
+						<section>
+							<div class="content">
+								<div class="inner">
+									<h2>Note My Problem</h2>
+									<p>Note My Problem was made at HackPSU by Xiaoyu Zhou, Chris Andrejewski, Rachel Rattay and myself. The project was the winner of the HackPSU Fall 2016 COIL/EdTech Challenge and the IBM - Watson Challenge. The application enables students to record a complete lecture transcript that not only contains the material covered in the lecture, but enhances the material through embedded links.
+									To view more check out our devpost page for the application.</p>
+									<a href="https://devpost.com/software/note-your-problem" target="_Blank" class="button">Devpost Page</a>
+								</div>
+							</div>
+							<a class="image"><img src="images/NoteMyProblem.jpg" alt=""/></a>
+						</section>
+					</section>
+
+				<!-- Three -->
+					<section id="four" class="wrapper style3 fade-up">
+						<div class="inner">
+							<h2>Contact me!</h2>
+							<p>Feel free to send me an email if you would like to see my official resume or have any questions for me!</p>
+							<div class="split style1">
+								<section>
+									<form method="post" action="thanks.php">
+										<div class="field half first">
+											<label for="name">Name</label>
+											<input type="text" name="name" id="name" />
+										</div>
+										<div class="field half">
+											<label for="email">Email</label>
+											<input type="text" name="email" id="email" />
+										</div>
+										<div class="field">
+											<label for="subject">Subject</label>
+											<input type="text" name="subject" id="subject" />
+										</div>
+										<div class="field">
+											<label for="message">Message</label>
+											<textarea name="message" id="message" rows="5"></textarea>
+										</div>
+										<ul class="actions">
+											<li><input href="/thanks.html" type="submit" name="submit" value="Submit" class="button submit"></li>
+										</ul>
+									</form>
+								</section>
+								<section>
+									<ul class="contact">
+										<li>
+											<h3>Email</h3>
+											<span>masontoy@gmail.com</span>
+										</li>
+										<li>
+											<h3>Resume</h3>
+											<a href="/masontoy_resume.docx" target="_Blank" class="button">Download</a>
+										</li>
+										<li>
+											<h3>Social</h3>
+											<ul class="icons">
+												<li><a href="https://www.facebook.com/mason.toy/" class="fa-facebook"><span class="label">Facebook</span></a></li>
+												<li><a href="https://github.com/MaSUB/" class="fa-github"><span class="label">GitHub</span></a></li>
+												<li><a href="https://www.linkedin.com/in/mason-toy-23bb71a2" class="fa-linkedin"><span class="label">LinkedIn</span></a></li>
+											</ul>
+										</li>
+									</ul>
+								</section>
+							</div>
+						</div>
+					</section>
+
+			</div>
+	</body>
+</html>
---
+++ thanks.php
@@ -0,0 +1,63 @@
+<?php 
+if(isset($_POST['submit'])){
+    $to = "mason.toy@gmail.com"; // this is your Email address
+    $from = $_POST['email']; // this is the sender's Email address
+    $name = $_POST['name'];
+    $subject = $_POST['subject'];
+    $message = "Name: " . $name . "\nEmail: " . $from .  "\n\n\n" . $_POST['message'];
+
+   	$headers = "From: mason@masontoy.com"; 
+    mail($to,$subject,$message,$headers);
+}
+?>
+<!DOCTYPE HTML>
+<!--
+	Hyperspace by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<title>Mason Toy</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<link rel="shortcut icon" type="image/x-icon" href="favicon.png" />
+		<!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
+		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+	</head>
+	<body>
+
+		<!-- Scripts -->
+		<script>
+		  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+		  ga('create', 'UA-86073703-1', 'auto');
+		  ga('send', 'pageview');
+
+		</script>
+		<script src="assets/js/jquery.min.js"></script>
+		<script src="assets/js/jquery.scrollex.min.js"></script>
+		<script src="assets/js/jquery.scrolly.min.js"></script>
+		<script src="assets/js/skel.min.js"></script>
+		<script src="assets/js/util.js"></script>
+		<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+		<script src="assets/js/main.js"></script>
+
+		<div id="wrapper">
+			<!-- Intro -->
+			<section id="intro" class="wrapper style1 fullscreen fade-up">
+				<div class="inner">
+					<h1><i class="fa fa-thumbs-up" aria-hidden="true"></i></h1>
+					<h1>Your email has been sent thanks!</h1>
+					<ul class="actions">
+						<li><a href="http://masontoy.com" class="button scrolly">Back Home</a></li>
+					</ul>
+				</div>
+			</section>
+		</div>
+	</body>
